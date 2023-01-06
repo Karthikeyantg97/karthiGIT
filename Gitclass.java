b0d39154227bf5a6e5f2d68553d5913e546cf8fd
karthikeyantg97 <karthikeyantg97@gmail.com>
2023-01-06 12:52:41 +0530

    msg

--- Gitclass.java
+++ Gitclass.java
@@ -2,15 +2,15 @@ package org.gitclass;
 
 public class Gitclass {
 
-	private void m1() {
+	private void m4() {
 		// TODO Auto-generated method stub
 
 	}
-	private void m2() {
+	private void m5() {
 		// TODO Auto-generated method stub
 
 	}
-	private void m3() {
+	private void m6() {
 		// TODO Auto-generated method stub
 
 	}
